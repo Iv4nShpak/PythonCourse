bfdac3ef07bc13a148440f1a1e4d6837d99e0c50
IShpak <84473401+Iv4nShpak@users.noreply.github.com>
2023-05-23 21:44:27 +0300

    add tasks from lesson #2

---
+++ lesson_2/task_9.py
@@ -0,0 +1,15 @@
+result = 1
+n = int(input())
+
+for i in range(1, n + 1):
+    result *= i
+
+print(result)
+
+# n = int(input('Введите целое число'))
+# factorial = 1
+#
+# for i in range(2, n + 1):
+#     factorial *= i
+#
+# print(i, '->', factorial)
